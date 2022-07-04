3f2afbbf4de0ea6950f3fef60d534a51771f8bf2
NANGGI <mqsora@naver.com>
2022-07-04 23:09:26 +0900

    Add files via upload

---
+++ loops/temp3.py
@@ -0,0 +1,10 @@
+Participants = ["Jen", "Alex", "Tina", "Joe", "Ben"]
+
+position = 1
+
+for name in Participants:
+  if name == "Tina":
+    break
+  position += 1
+  
+print(position)
---
+++ loops/temp5.py
@@ -0,0 +1,5 @@
+for row in range(5):
+  if row % 2 == 0:
+    print(" | | ")
+  else:
+    print("-----")
